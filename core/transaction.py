01701c818b521c0f410ed0334910234d435e09b4
TamtiGior <tgior20@freeuni.edu.ge>
2024-02-04 20:41:53 +0400

    transaction modified

--- core/transaction.py
+++ core/transaction.py
@@ -1,34 +1,21 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Protocol
-from uuid import UUID, uuid4
-
-from core.constants import SATOSHIS_IN_ONE_BITCOIN, TRANSACTION_FEE
+from uuid import UUID
 
 
 @dataclass
-class Transaction(Protocol):
+class ITransaction(Protocol):
     from_wallet_address: UUID
     to_wallet_address: UUID
-    amount: float
-    transaction_id: UUID
-
-    def calculate_transaction_fee(self, wallets_database: WalletsRepository) -> float:
-        pass
+    sent_amount: float
+    fee_amount: float
+    total_amount: float
 
 
 @dataclass
-class StandardTransaction(Protocol):
+class Transaction(ITransaction):
     from_wallet_address: UUID
     to_wallet_address: UUID
-    amount: float
-    transaction_id: UUID = field(default_factory=uuid4)
-
-    def calculate_transaction_fee(self, wallets_database: WalletsRepository) -> float:
-        from_owner = wallets_database.get_owner_id(self.from_wallet_address)
-        to_owner = wallets_database.get_owner_id(self.to_wallet_address)
-        if from_owner == to_owner:
-            return 0
-
-        amount_satoshis = self.amount * SATOSHIS_IN_ONE_BITCOIN
-        transaction_fee = max(1, amount_satoshis * TRANSACTION_FEE)
-        return transaction_fee / SATOSHIS_IN_ONE_BITCOIN
+    sent_amount: float
+    fee_amount: float
+    total_amount: float
