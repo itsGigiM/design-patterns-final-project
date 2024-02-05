884c8593562baaf07572e65a976630c49219423c
TamtiGior <tgior20@freeuni.edu.ge>
2024-02-06 02:30:28 +0400

    mypy half done

---
+++ core/walletToWalletUSDAdapter.py
@@ -0,0 +1,23 @@
+from typing import Protocol
+
+from core.BTCtoUSDconverter import IBTCtoUSDConverter
+from core.wallet import IWallet, WalletUSD
+
+
+class IWalletToWalletUSDAdapter(Protocol):
+    def convert(self, wallet: IWallet) -> WalletUSD:
+        pass
+
+
+class WalletToUSDWalletAdapter(IWalletToWalletUSDAdapter):
+    def __init__(self, converter: IBTCtoUSDConverter):
+        self.converter = converter
+
+    def convert(self, wallet: IWallet) -> WalletUSD:
+        usd_value = self.converter.convert(wallet.amount)
+        return WalletUSD(
+            usd_amount=usd_value,
+            api_key=wallet.api_key,
+            address=wallet.address,
+            amount=wallet.amount,
+        )
