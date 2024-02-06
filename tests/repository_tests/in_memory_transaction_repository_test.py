88804977e1af19e7d0ca9e6b5e417f4a11b398de
Mariam Ratiani <mrati20@freeuni.edu.ge>
2024-02-06 17:33:35 +0400

    wrote in memory transaction tests

--- tests/repository_tests/in_memory_transaction_repository_test.py
+++ tests/repository_tests/in_memory_transaction_repository_test.py
@@ -2,29 +2,30 @@ from typing import Any
 
 import pytest
 
+from core.repository_interface.transaction_repository_interface import ITransactionRepository
 from infra.repository.database_executor import DatabaseExecutor
 from infra.repository.transaction_repository import InMemoryTransactionRepository
 
 
 @pytest.fixture
-def transaction_repo(db_executor: DatabaseExecutor) -> InMemoryTransactionRepository:
+def transaction_repo(db_executor: DatabaseExecutor) -> ITransactionRepository:
     transaction_repo = InMemoryTransactionRepository()
     transaction_repo.create_table()
     return transaction_repo
 
 
-valid_uuid1 = "12345678-1234-5678-1234-567812345678"
-valid_uuid2 = "87654321-4321-6789-4321-987654321098"
+FROM = "12345678-1234-5678-1234-567812345678"
+TO = "87654321-4321-6789-4321-987654321098"
 
 
-def test_create_Transaction(transaction_repo: InMemoryTransactionRepository) -> None:
-    assert transaction_repo.create_transaction(valid_uuid1, valid_uuid2, 100, 5)
+def test_create_Transaction(transaction_repo: ITransactionRepository) -> None:
+    assert transaction_repo.create_transaction(FROM, TO, 100, 5)
 
 
 def compare(result: Any):
     expected_results = [
-        (valid_uuid1, valid_uuid2, 100, 5, 105),
-        (valid_uuid1, valid_uuid1, 20, 5, 25)
+        (FROM, TO, 100, 5, 105),
+        (FROM, FROM, 20, 5, 25)
     ]
 
     assert len(result) == len(expected_results)
@@ -36,48 +37,48 @@ def compare(result: Any):
         assert transaction.total_amount == expected[4]
 
 
-def test_get_all_transactions(transaction_repo: InMemoryTransactionRepository) -> None:
+def test_get_all_transactions(transaction_repo: ITransactionRepository) -> None:
     result = transaction_repo.get_all_transactions()
     assert result == []
 
-    transaction_repo.create_transaction(valid_uuid1, valid_uuid2, 100, 5)
-    transaction_repo.create_transaction(valid_uuid1, valid_uuid1, 20, 5)
+    transaction_repo.create_transaction(FROM, TO, 100, 5)
+    transaction_repo.create_transaction(FROM, FROM, 20, 5)
 
     result = transaction_repo.get_all_transactions()
 
     assert len(result) == 2
 
     # Check details of the first transaction
-    assert str(result[0].from_wallet_address) == valid_uuid1
-    assert str(result[0].to_wallet_address) == valid_uuid2
+    assert str(result[0].from_wallet_address) == FROM
+    assert str(result[0].to_wallet_address) == TO
     assert result[0].sent_amount == 100
     assert result[0].fee_amount == 5
     assert result[0].total_amount == 105
 
     # Check details of the second transaction
-    assert str(result[1].from_wallet_address) == valid_uuid1
-    assert str(result[1].to_wallet_address) == valid_uuid1
+    assert str(result[1].from_wallet_address) == FROM
+    assert str(result[1].to_wallet_address) == FROM
     assert result[1].sent_amount == 20
     assert result[1].fee_amount == 5
     assert result[1].total_amount == 25
 
 
 def test_get_wallet_all_transactions(
-        transaction_repo: InMemoryTransactionRepository,
+        transaction_repo: ITransactionRepository,
 ) -> None:
-    result = transaction_repo.get_wallet_all_transactions(valid_uuid2)
+    result = transaction_repo.get_wallet_all_transactions(TO)
     assert result == []
-    transaction_repo.create_transaction(valid_uuid1, valid_uuid2, 100, 5)
-    transaction_repo.create_transaction(valid_uuid1, valid_uuid2, 20, 5)
-    result = transaction_repo.get_wallet_all_transactions(valid_uuid2)
+    transaction_repo.create_transaction(FROM, TO, 100, 5)
+    transaction_repo.create_transaction(FROM, TO, 20, 5)
+    result = transaction_repo.get_wallet_all_transactions(TO)
     compare(result)
 
 
-def test_get_statistics(transaction_repo: InMemoryTransactionRepository) -> None:
+def test_get_statistics(transaction_repo: ITransactionRepository) -> None:
     transaction_repo.drop_table()
     transaction_repo.create_table()
-    transaction_repo.create_transaction(valid_uuid1, valid_uuid2, 100, 5)  # Total amount 105, fee 5
-    transaction_repo.create_transaction(valid_uuid1, valid_uuid2, 20, 5)  # Total amount 25, fee 5
+    transaction_repo.create_transaction(FROM, TO, 100, 5)  # Total amount 105, fee 5
+    transaction_repo.create_transaction(FROM, TO, 20, 5)  # Total amount 25, fee 5
     result = transaction_repo.get_statistics()
 
     assert result["transaction_total_number"] == 2
