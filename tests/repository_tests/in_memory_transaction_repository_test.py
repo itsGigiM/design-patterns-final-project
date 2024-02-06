b0d28171001840acddd74d2f35d883247fdf8d52
Mariam Ratiani <mrati20@freeuni.edu.ge>
2024-02-06 18:10:15 +0400

    found bug in in mem transaction test

--- tests/repository_tests/in_memory_transaction_repository_test.py
+++ tests/repository_tests/in_memory_transaction_repository_test.py
@@ -2,13 +2,11 @@ from typing import Any
 
 import pytest
 
-from core.repository_interface.transaction_repository_interface import ITransactionRepository
-from infra.repository.database_executor import DatabaseExecutor
 from infra.repository.transaction_repository import InMemoryTransactionRepository
 
 
 @pytest.fixture
-def transaction_repo(db_executor: DatabaseExecutor) -> ITransactionRepository:
+def transaction_repo() -> InMemoryTransactionRepository:
     transaction_repo = InMemoryTransactionRepository()
     transaction_repo.create_table()
     return transaction_repo
@@ -18,26 +16,11 @@ FROM = "12345678-1234-5678-1234-567812345678"
 TO = "87654321-4321-6789-4321-987654321098"
 
 
-def test_create_Transaction(transaction_repo: ITransactionRepository) -> None:
+def test_create_transaction(transaction_repo: InMemoryTransactionRepository) -> None:
     assert transaction_repo.create_transaction(FROM, TO, 100, 5)
 
 
-def compare(result: Any):
-    expected_results = [
-        (FROM, TO, 100, 5, 105),
-        (FROM, FROM, 20, 5, 25)
-    ]
-
-    assert len(result) == len(expected_results)
-    for transaction, expected in zip(result, expected_results):
-        assert str(transaction.from_wallet_address) == expected[0]
-        assert str(transaction.to_wallet_address) == expected[1]
-        assert transaction.sent_amount == expected[2]
-        assert transaction.fee_amount == expected[3]
-        assert transaction.total_amount == expected[4]
-
-
-def test_get_all_transactions(transaction_repo: ITransactionRepository) -> None:
+def test_get_all_transactions(transaction_repo: InMemoryTransactionRepository) -> None:
     result = transaction_repo.get_all_transactions()
     assert result == []
 
@@ -64,17 +47,28 @@ def test_get_all_transactions(transaction_repo: ITransactionRepository) -> None:
 
 
 def test_get_wallet_all_transactions(
-        transaction_repo: ITransactionRepository,
+        transaction_repo: InMemoryTransactionRepository,
 ) -> None:
     result = transaction_repo.get_wallet_all_transactions(TO)
     assert result == []
     transaction_repo.create_transaction(FROM, TO, 100, 5)
     transaction_repo.create_transaction(FROM, TO, 20, 5)
     result = transaction_repo.get_wallet_all_transactions(TO)
-    compare(result)
+    expected_results = [
+        (FROM, TO, 100, 5, 105),
+        (FROM, TO, 20, 5, 25)
+    ]
+
+    assert len(result) == len(expected_results)
+    for transaction, expected in zip(result, expected_results):
+        assert str(transaction.from_wallet_address) == expected[0]
+        assert str(transaction.to_wallet_address) == expected[1]
+        assert transaction.sent_amount == expected[2]
+        assert transaction.fee_amount == expected[3]
+        assert transaction.total_amount == expected[4]
 
 
-def test_get_statistics(transaction_repo: ITransactionRepository) -> None:
+def test_get_statistics(transaction_repo: InMemoryTransactionRepository) -> None:
     transaction_repo.drop_table()
     transaction_repo.create_table()
     transaction_repo.create_transaction(FROM, TO, 100, 5)  # Total amount 105, fee 5
